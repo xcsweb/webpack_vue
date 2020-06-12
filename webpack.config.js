bf559c0c61e392b78208254153494b4ba6ba48ed
a1541863207@gmail.com <a1541863207@gmail.com>
2020-06-12 23:09:52 +0800

    最新处理

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const {resolve} =require("path");
 const path=require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin= require('optimize-css-assets-webpack-plugin' );
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack =require("webpack");
+// const mode=process.argv[4]||"development";
+const mode="production"
+// process.env.NODE_ENV=mode;
+// process.env.NODE_ENV
+let styleLoader=null;
+let extract=null;
+if(mode==="development"){
+    styleLoader="style-loader"
+    extract= true;
+}else{
+    styleLoader=MiniCssExtractPlugin.loader;
+    extract=false;
+}
+console.log(styleLoader)
+let styleArr=[ styleLoader,
+    'css-loader',
+    { loader: 'postcss-loader', options: { ident: 'postcss', plugins: () => [ require('postcss-preset-env')()  ] } }]
 module.exports={
-    mode:"development",
+    mode:mode,
     entry:"./src/main.js",
     output:{
         filename:"built.js",
@@ -14,16 +33,12 @@ module.exports={
         rules:[
             {
                 test:/\.css$/,
-                use:[
-                    "style-loader",
-                    "css-loader"
-                ]
+                use:styleArr
             },
             {
                 test:/\.scss$/,
                 use:[
-                    "style-loader",
-                    "css-loader",
+                    ...styleArr,
                     "sass-loader"
                 ]
             },
@@ -48,12 +63,15 @@ module.exports={
         }
     },
     plugins:[
+        new MiniCssExtractPlugin({ filename: 'css/built.css' }), 
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             template:"./public/index.html"
         }),
-        new webpack.SourceMapDevToolPlugin({})
+        new webpack.SourceMapDevToolPlugin({}),
+        new OptimizeCssAssetsWebpackPlugin(),
     ],
+    optimization: { splitChunks: { chunks: 'all' }},
     devtool:"eval-source-map",
     devServer:{
         contentBase:resolve(__dirname,"build"),
